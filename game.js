fd9b4a76c242ba410c886a034628833fe2ec7c94
peter <hofstedemail@yahoo.com>
2015-10-18 21:32:39 +0200

    mouse input

--- game.js
+++ game.js
@@ -1,11 +1,22 @@
 
 
 
-game = new Game("gamecanvas");
+var game = new Game("gamecanvas");
+//TODO expected name, not texture
 
-var star = new GameObject("star");
-star.Update = function()
+
+for (var i=0;i<10;i++)
 {
-	this.transform.position = game.input.mousePosition;
-}
 
+	var star = new GameObject("star");
+
+	//TODO Make vector random unit circle util
+	star.transform.position = Random.InsideUnitCircle();
+
+	star.Update = function ()
+	{
+		//this.transform.position = this.transform.position.Lerp(game.input.mousePosition, i*0.1);
+		this.transform.rotation += 10;
+	}
+
+}
--- gameengine.js
+++ gameengine.js
@@ -14,7 +14,7 @@ function GameObject(textureid)
     //    this.texture = document.getElementById(id);
     //}
 
-    game.gameObjects.push(this);
+    Game.instance.gameObjects.push(this);
 
     this.Update = function() {};
 
@@ -29,6 +29,18 @@ function Transform()
 	this.scale = new Vector2();
 }
 
+function Random()
+{
+    Random.InsideUnitCircle = function()
+    {
+        return new Vector2(Math.random(),Math.random())
+    }
+
+    Random.InsideRect = function(x,y,width,height)
+    {
+        return new Vector2(x + width*Math.random(),y + height*Math.random())
+    }
+}
 
 function Vector2(x,y)
 {
@@ -39,11 +51,44 @@ function Vector2(x,y)
     {
         return new Vector2(0,0);
     }
+
+    this.Add = function(vec)
+    {
+        return new Vector2(this.x+vec.x,this.y+vec.y);
+    }
+
+
+    this.Subtract = function(vec)
+    {
+        return new Vector2(this.x-vec.x,this.y-vec.y);
+    }
+
+    this.Multiply = function(amount)
+    {
+        return new Vector2(amount*x,amount*y)
+
+    }
+
+    this.Divide = function(amount)
+    {
+        return new Vector2(x/amount,y/amount)
+    }
+
+
+    this.Lerp = function(vec,fraction)
+    {
+        var resultvec = new Vector2();
+        resultvec.x = (vec.x - this.x) * fraction;
+        resultvec.y = (vec.y - this.y) * fraction;
+        return this.Add(resultvec);
+    }
+
 }
 
 
 function Game(canvas)
 {
+    Game.instance = this;
 
     this.canvas = document.getElementById(canvas);
     this.ctx = this.canvas.getContext("2d");
@@ -56,8 +101,8 @@ function Game(canvas)
     this.canvas.onmousemove = function(e)
     {
 
-        game.input.mousePosition.x = e.clientX - this.offsetLeft;
-        game.input.mousePosition.y = e.clientY - this.offsetTop;
+        Game.instance.input.mousePosition.x = e.clientX - this.offsetLeft;
+        Game.instance.input.mousePosition.y = e.clientY - this.offsetTop;
     }
 
     //keep track of all gameobject and kickoff the gameloop
@@ -69,6 +114,8 @@ function Game(canvas)
         document.getElementById("output").innerHTML = str;
     }
 
+    return this;
+
 }
 
 function Input()
@@ -79,14 +126,18 @@ function Input()
 function GameloopUpdate()
 {
 
+    var ctx = Game.instance.ctx;
+
     for (var i=0;i<game.gameObjects.length;i++)
     {
         var go = game.gameObjects[i];
-        game.ctx.save();
-        game.ctx.clearRect(0,0,game.width,game.height);
-        game.ctx.translate(go.transform.position.x,go.transform.position.y);
-        game.ctx.drawImage(go.texture,-go.texture.naturalWidth/2,-go.texture.naturalHeight/2);
-        game.ctx.restore();
+        ctx.save();
+        ctx.clearRect(0,0,game.width,game.height);
+        ctx.translate(go.transform.position.x,go.transform.position.y);
+        ctx.rotate(go.transform.rotation * Math.PI / 180);
+        ctx.drawImage(go.texture,-go.texture.naturalWidth/2,-go.texture.naturalHeight/2);
+        ctx.restore();
+
         go.Update();
     }
 
